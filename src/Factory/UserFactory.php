dbdd302c80c1e80a5690279765c802e1c9e434b5
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-11-30 18:15:43 +0100

    Modification du code des méthodes du UserFactory pour qu'il corresponde à la documentation.

--- src/Factory/UserFactory.php
+++ src/Factory/UserFactory.php
@@ -4,36 +4,39 @@ namespace App\Factory;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use Zenstruck\Foundry\RepositoryProxy;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Zenstruck\Foundry\ModelFactory;
 use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
 
 /**
  * @extends ModelFactory<User>
  *
- * @method static User|Proxy createOne(array $attributes = [])
- * @method static User[]|Proxy[] createMany(int $number, array|callable $attributes = [])
- * @method static User[]|Proxy[] createSequence(array|callable $sequence)
- * @method static User|Proxy find(object|array|mixed $criteria)
- * @method static User|Proxy findOrCreate(array $attributes)
- * @method static User|Proxy first(string $sortedField = 'id')
- * @method static User|Proxy last(string $sortedField = 'id')
- * @method static User|Proxy random(array $attributes = [])
- * @method static User|Proxy randomOrCreate(array $attributes = [])
- * @method static User[]|Proxy[] all()
- * @method static User[]|Proxy[] findBy(array $attributes)
- * @method static User[]|Proxy[] randomSet(int $number, array $attributes = [])
- * @method static User[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static User|Proxy                     createOne(array $attributes = [])
+ * @method static User[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static User[]|Proxy[]                 createSequence(array|callable $sequence)
+ * @method static User|Proxy                     find(object|array|mixed $criteria)
+ * @method static User|Proxy                     findOrCreate(array $attributes)
+ * @method static User|Proxy                     first(string $sortedField = 'id')
+ * @method static User|Proxy                     last(string $sortedField = 'id')
+ * @method static User|Proxy                     random(array $attributes = [])
+ * @method static User|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static User[]|Proxy[]                 all()
+ * @method static User[]|Proxy[]                 findBy(array $attributes)
+ * @method static User[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ * @method static User[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
  * @method static UserRepository|RepositoryProxy repository()
- * @method User|Proxy create(array|callable $attributes = [])
+ * @method        User|Proxy                     create(array|callable $attributes = [])
  */
 final class UserFactory extends ModelFactory
 {
-    public function __construct()
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         parent::__construct();
 
-        // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+        $this->passwordHasher = $passwordHasher;
     }
 
     protected function getDefaults(): array
@@ -48,9 +51,10 @@ final class UserFactory extends ModelFactory
 
     protected function initialize(): self
     {
-        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
         return $this
-            // ->afterInstantiate(function(User $user): void {})
+            ->afterInstantiate(function (User $user) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            })
         ;
     }
 
