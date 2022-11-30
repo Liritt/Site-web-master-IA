a6468a6da0da5865b1b985f68ef25ac29c04f890
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-11-30 18:29:23 +0100

    Ajout d'une redirection après la conenxion de l'utilisateur.

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -11,8 +11,8 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         UserFactory::createSequence([
-            ['email' => 'admin@example.com', 'roles' => ['ROLE_ADMIN'], 'password' => 'admin', 'firstname' => 'Evan', 'lastname' => 'Pluchart'],
-            ['email' => 'user@example.com', 'roles' => ['ROLE_USER'], 'password' => 'test', 'firstname' => 'Gaetan', 'lastname' => 'Lemont'],
+            ['email' => 'admin@example.com', 'roles' => ['ROLE_ADMIN'], 'password' => 'admin'],
+            ['email' => 'user@example.com', 'roles' => ['ROLE_USER'], 'password' => 'test'],
         ]);
         UserFactory::createMany(10);
     }
