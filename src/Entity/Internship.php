684ee679af261056d0a0e9da5ea9f5bc5685d02f
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-12-03 13:54:06 +0100

    Création de l'entité intership.

---
+++ src/Entity/Internship.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InternshipRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InternshipRepository::class)]
+class Internship
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $beginDate = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $endDate = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $city = null;
+
+    #[ORM\Column]
+    private ?int $studentsNumber = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $subject = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBeginDate(): ?\DateTimeInterface
+    {
+        return $this->beginDate;
+    }
+
+    public function setBeginDate(\DateTimeInterface $beginDate): self
+    {
+        $this->beginDate = $beginDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getStudentsNumber(): ?int
+    {
+        return $this->studentsNumber;
+    }
+
+    public function setStudentsNumber(int $studentsNumber): self
+    {
+        $this->studentsNumber = $studentsNumber;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+}
