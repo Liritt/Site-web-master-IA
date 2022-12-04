323f2c797e4184d73a06729e2160b3f02eb1b43d
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-12-04 13:14:19 +0100

    Ajout du formulaire de suppression de stage.

--- src/Controller/InternshipController.php
+++ src/Controller/InternshipController.php
@@ -7,6 +7,7 @@ use App\Form\InternshipType;
 use App\Repository\InternshipRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -59,5 +60,29 @@ class InternshipController extends AbstractController
         }
 
         return $this->renderForm('internship/create.html.twig', ['form' => $form]);
+    }
+
+    #[Route('/internship/{id}/delete', name: 'app_internship_delete', requirements: ['id' => '\d+'])]
+    public function delete(Request $request, ManagerRegistry $doctrine, Internship $internship): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $form = $this->createFormBuilder($internship)
+            ->add('delete', SubmitType::class, ['label' => 'Supprimer', 'attr' => ['class' => 'btn btn-primary']])
+            ->add('cancel', SubmitType::class, ['label' => 'Annuler', 'attr' => ['class' => 'btn btn-secondary']])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->getClickedButton() && 'delete' === $form->getClickedButton()->getName()) {
+            $entityManager->remove($internship);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_internship');
+        }
+
+        if ($form->getClickedButton() && 'cancel' === $form->getClickedButton()->getName()) {
+            return $this->redirectToRoute('app_internship_show', ['id' => $internship->getId()]);
+        }
+
+        return $this->renderForm('internship/delete.html.twig', ['contact' => $internship, 'form' => $form]);
     }
 }
