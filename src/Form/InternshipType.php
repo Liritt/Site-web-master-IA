2c6b7733aa11ab5b95d08443b952a147776f6087
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-12-04 12:41:39 +0100

    Modification des attributs des champs du formulaire InternshipType.

--- src/Form/InternshipType.php
+++ src/Form/InternshipType.php
@@ -2,22 +2,54 @@
 
 namespace App\Form;
 
+use App\Entity\Company;
 use App\Entity\Internship;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
 
 class InternshipType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('beginDate')
-            ->add('endDate')
-            ->add('city')
-            ->add('studentsNumber')
-            ->add('subject')
-            ->add('company')
+            ->add('beginDate', DateType::class, [
+                'widget' => 'single_text',
+                'html5' => false,
+                'format' => 'dd/MM/yyyy',
+                'attr' => [
+                    'class' => 'js-datepicker',
+                ],
+            ])
+            ->add('endDate', DateType::class, [
+                'widget' => 'single_text',
+                'html5' => false,
+                'format' => 'dd/MM/yyyy',
+                'attr' => [
+                    'class' => 'js-datepicker',
+                ],
+            ])
+            ->add('city', null, [
+                'empty_data' => '',
+            ])
+            ->add('studentsNumber', null, [
+                'empty_data' => '',
+            ])
+            ->add('subject', null, [
+                'empty_data' => '',
+            ])
+            ->add('company', EntityType::class, [
+                'class' => Company::class,
+                'choice_label' => 'name',
+                'query_builder' => function (EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
+            ])
         ;
     }
 
