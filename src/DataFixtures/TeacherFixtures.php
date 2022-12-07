e3ada36845a90b9309ffb264cca5a6ab9b35d94a
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-12-07 10:46:33 +0100

    Ajout d'utilisateurs tester pour les Company, Student et Teacher.

--- src/DataFixtures/TeacherFixtures.php
+++ src/DataFixtures/TeacherFixtures.php
@@ -10,6 +10,13 @@ class TeacherFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        TeacherFactory::createOne([
+            'email' => 'teacher@example.com',
+            'roles' => ['ROLE_TEACHER'],
+            'password' => 'test',
+            'firstname' => 'Teacher',
+            'lastname' => 'Tester'
+        ]);
         TeacherFactory::createMany(10);
     }
 }
