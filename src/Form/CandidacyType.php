cee5ac9172ca46c324f4a253dcbef8b06b563e56
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-12-07 11:29:50 +0100

    Paramétrage du CandidacyType form.

--- src/Form/CandidacyType.php
+++ src/Form/CandidacyType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Candidacy;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,10 @@ class CandidacyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('cv')
-            ->add('coverLetter')
-            ->add('student')
-            ->add('internship')
+            ->add('cv', FileType::class, [
+                'label' => 'File'
+            ])
+            ->add('Cover Letter', TextType::class)
         ;
     }
 
