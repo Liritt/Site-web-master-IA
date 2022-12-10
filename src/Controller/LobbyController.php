6e6986935604ff739303e338e608569c4553e097
alban <alban.robion@etudiant.univ-reims.fr>
2022-12-10 10:20:02 +0100

    modif du header pour ajouter le dropdown avec les drapeaux modification des routes pour ajout des langues

--- src/Controller/LobbyController.php
+++ src/Controller/LobbyController.php
@@ -8,9 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LobbyController extends AbstractController
 {
-    #[Route('/', name: 'app_lobby')]
+    #[Route('/{_locale<%app.supported_locales%>}/', name: 'app_lobby_index')]
     public function index(): Response
     {
         return $this->render('lobby/index.html.twig');
+    }
+
+    #[Route('/')]
+    public function indexNoLocale(): Response
+    {
+        return $this->redirectToRoute('app_lobby_index', ['_locale' => 'fr']);
     }
 }
