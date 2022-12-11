64c679397ea2272c49c4689d255d01ca0f98b513
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2022-12-11 14:49:44 +0100

    Correction de la route de redirection du formulaire de connexion.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -47,7 +47,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_lobby'));
+        return new RedirectResponse($this->urlGenerator->generate('app_lobby_index'));
     }
 
     protected function getLoginUrl(Request $request): string
