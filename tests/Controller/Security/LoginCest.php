8e320bf9ca8f4a420f5784e270a2278a6ba270e2
alban <alban.robion@etudiant.univ-reims.fr>
2022-12-12 19:58:07 +0100

    mise en place des tests et ajout d'un premier test pour tester le test

--- tests/Controller/Security/LoginCest.php
+++ tests/Controller/Security/LoginCest.php
@@ -1,14 +1,27 @@
 <?php
 
-
 namespace App\Tests\Controller\Security;
 
-use App\ControllerTester;
+use App\Factory\UserFactory;
+use App\Tests\Support\ControllerTester;
 
 class LoginCest
 {
     public function LoginAdmin(ControllerTester $I)
     {
+        $user = UserFactory::createOne([
+            'email' => 'user@test.com',
+            'password' => 'test',
+            'roles' => ['ROLE_STUDENT'],
+        ]);
+
+        $realUser = $user->object();
+
+        $I->amLoggedInAs($realUser);
+
+        $I->amOnPage('/login');
+
+        $I->seeInTitle('Log in!');
     }
 
     public function LoginStudent(ControllerTester $I)
