8b02850cb42077232871e42a4d5d3d8791115cde
Lirit <gaetanl2004@gmail.com>
2022-12-18 22:35:52 +0100

    Génération des TER avec Faker fonctionnelle

--- src/DataFixtures/TERFixtures.php
+++ src/DataFixtures/TERFixtures.php
@@ -2,17 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\TeacherFactory;
 use App\Factory\TERFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class TERFixtures extends Fixture
+class TERFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        TERFactory::createMany(10, function () {
+            return [
+                'teacher' => TeacherFactory::random(),
+            ];
+        });
+    }
 
-        TERFactory::createMany(10);
+    public function getDependencies(): array
+    {
+        return [
+            TeacherFixtures::class,
+        ];
     }
 }
