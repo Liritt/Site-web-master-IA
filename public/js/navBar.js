8bc8b97e20187d939fdb30c57445d866c3dccd10
alban <alban.robion@etudiant.univ-reims.fr>
2022-12-20 16:04:01 +0100

    autres test

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -4,8 +4,8 @@ const onglets = container.getElementsByClassName("accueil-link");
 
 for (let i = 0; i < onglets.length; i++) {
     onglets[i].addEventListener("click", function() {
-        const current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
+        const current = document.getElementsByClassName("test");
+        current[0].className = current[0].className.replace(" test", "");
+        this.className += " test";
     });
 }
