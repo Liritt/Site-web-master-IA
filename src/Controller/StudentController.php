e44175b2922b58d13b65d2ca3d555b687a7feb89
laid0003 <lucie.laidie@etudiant.univ-reims.fr>
2023-01-05 13:58:04 +0100

    Modification du controller pour recuperer les etudiants

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class StudentController extends AbstractController
 {
     #[Route('/student', name: 'app_student')]
-    public function index(): Response
+    public function index(StudentRepository $repository): Response
     {
-        return $this->render('student/index.html.twig', [
-            'controller_name' => 'StudentController',
-        ]);
+        $students = $repository->findAll();
+
+        return $this->render('student/index.html.twig', ['students' => $students]);
     }
 }
