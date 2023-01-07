fcafff5aeeffa61b8957ac2d666f0757c74ff8fb
lemo0037 <gaetan.lemont@etudiant.univ-reims.fr>
2023-01-07 20:36:56 +0100

    Création de la fonction handleDragStart

---
+++ public/js/ter.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    function handleDragStart(e) {
+        this.style.opacity = '0.4';
+
+        dragSrcEl = this;
+
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+});
