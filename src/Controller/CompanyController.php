9839f4cd2e067660139829b0106d15ece18bd579
laid0003 <lucie.laidie@etudiant.univ-reims.fr>
2023-01-07 17:13:13 +0100

    Modification du controlleur pour ajouter la recherche

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -4,15 +4,18 @@ namespace App\Controller;
 
 use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CompanyController extends AbstractController
 {
     #[Route('/company', name: 'app_company')]
-    public function index(CompanyRepository $repository): Response
+    public function index(CompanyRepository $repository, Request $request): Response
     {
-        $companys = $repository->findAll();
-        return $this->render('company/index.html.twig', ['companys' => $companys]);
+        $search = $request->query->get('search', '');
+        $companys = $repository->search($search);
+
+        return $this->render('company/index.html.twig', ['companys' => $companys, 'search' => $search]);
     }
 }
