2e8b4f8a71e5e5c65e9af610be1f73fb67f420cc
laid0003 <lucie.laidie@etudiant.univ-reims.fr>
2023-01-07 12:58:33 +0100

    Modification du controlleur pour recuperer tous les profs

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TeacherRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class TeacherController extends AbstractController
 {
     #[Route('/teacher', name: 'app_teacher')]
-    public function index(): Response
+    public function index(TeacherRepository $repository): Response
     {
-        return $this->render('teacher/index.html.twig', [
-            'controller_name' => 'TeacherController',
-        ]);
+        $teachers = $repository->findAll();
+        return $this->render('teacher/index.html.twig', ['teachers' => $teachers]);
     }
 }
