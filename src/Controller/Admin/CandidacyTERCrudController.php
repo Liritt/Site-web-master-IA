38b2f259802b21d6fc110e7629910256aaa47d15
alban <alban.robion@etudiant.univ-reims.fr>
2023-01-08 12:02:08 +0100

    modif admin: modif pas importante de style

--- src/Controller/Admin/CandidacyTERCrudController.php
+++ src/Controller/Admin/CandidacyTERCrudController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 
 class CandidacyTERCrudController extends AbstractCrudController
 {
@@ -35,6 +36,32 @@ class CandidacyTERCrudController extends AbstractCrudController
             AssociationField::new('TER')
                 ->formatValue(function ($value, $entity) {
                     return $entity->getTER()->getTitle();
+                }),
+            IntegerField::new('orderNumber')
+                ->setLabel('Ordre')
+                ->formatValue(function ($value, $entity) {
+                    $order = $entity->getOrderNumber();
+                    if (1 == $order) {
+                        return '<span class="material-icons">looks_one</span>';
+                    } elseif (2 == $order) {
+                        return '<span class="material-icons">looks_two</span>';
+                    } elseif (3 == $order) {
+                        return '<span class="material-icons">looks_3</span>';
+                    } elseif (4 == $order) {
+                        return '<span class="material-icons">looks_4</span>';
+                    } elseif (5 == $order) {
+                        return '<span class="material-icons">looks_5</span>';
+                    } elseif (6 == $order) {
+                        return '<span class="material-icons">looks_6</span>';
+                    } elseif (7 == $order) {
+                        return '<span class="material-icons">looks_7</span>';
+                    } elseif (8 == $order) {
+                        return '<span class="material-icons">looks_8</span>';
+                    } elseif (9 == $order) {
+                        return '<span class="material-icons">looks_9</span>';
+                    } else {
+                        return '';
+                    }
                 }),
         ];
     }
