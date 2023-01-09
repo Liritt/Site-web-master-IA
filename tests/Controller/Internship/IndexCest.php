ee513eaf1ca18df8f3a8fef96bea74b63afaa10d
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-01-09 23:53:02 +0100

    test: Ajout du test checkAddInternshipButton vérifiant la présence du bouton d'ajout de stage pour une entreprise.

--- tests/Controller/Internship/IndexCest.php
+++ tests/Controller/Internship/IndexCest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller\Internship;
 
 use App\Factory\AdministratorFactory;
+use App\Factory\CompanyFactory;
 use App\Factory\InternshipFactory;
 use App\Tests\Support\ControllerTester;
 
@@ -36,5 +37,20 @@ class IndexCest
         $I->seeInTitle('Liste des stages');
         $I->see('Liste des stages', 'h1');
         $I->seeNumberOfElements('.card-body', 5);
+    }
+
+    public function checkAddInternshipButton(ControllerTester $I): void
+    {
+        InternshipFactory::createMany(5);
+        $user = CompanyFactory::createOne([
+            'email' => 'company@example.com',
+            'password' => 'test',
+            'roles' => ['ROLE_COMPANY'],
+        ]);
+        $realUser = $user->object();
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/fr/internship');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeNumberOfElements('.index__addInternshipBtn', 1);
     }
 }
