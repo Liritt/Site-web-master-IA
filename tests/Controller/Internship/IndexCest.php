01cfb8de1313320dac335cb6d6e8c00fc0448863
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-01-09 23:48:46 +0100

    test: Ajout du test checkContent vérifiant la présence des div contenant les stages.

--- tests/Controller/Internship/IndexCest.php
+++ tests/Controller/Internship/IndexCest.php
@@ -20,4 +20,21 @@ class IndexCest
         $I->amOnPage('/fr/internship');
         $I->seeResponseCodeIsSuccessful();
     }
+
+    public function checkContent(ControllerTester $I): void
+    {
+        InternshipFactory::createMany(5);
+        $user = AdministratorFactory::createOne([
+            'email' => 'admin@example.com',
+            'password' => 'admin',
+            'roles' => ['ROLE_ADMIN'],
+        ]);
+        $realUser = $user->object();
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/fr/internship');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInTitle('Liste des stages');
+        $I->see('Liste des stages', 'h1');
+        $I->seeNumberOfElements('.card-body', 5);
+    }
 }
