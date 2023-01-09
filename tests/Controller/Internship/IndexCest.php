3a425c3f6c6ce7b0f9979ed829a80ddbe2f536e3
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-01-09 23:45:17 +0100

    fix: Changement du nom du fichier de test.

---
+++ tests/Controller/Internship/IndexCest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Controller\Internship;
+
+use App\Factory\AdministratorFactory;
+use App\Factory\InternshipFactory;
+use App\Tests\Support\ControllerTester;
+
+class IndexCest
+{
+}
