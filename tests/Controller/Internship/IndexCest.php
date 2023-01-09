5edd4caeef6cee7642904e5dd77f37459fd70f19
pluc0003 <evan.pluchart@etudiant.univ-reims.fr>
2023-01-09 23:46:01 +0100

    test: Ajout du test isRouteValid permettant la vérification de la route index.

--- tests/Controller/Internship/IndexCest.php
+++ tests/Controller/Internship/IndexCest.php
@@ -8,4 +8,16 @@ use App\Tests\Support\ControllerTester;
 
 class IndexCest
 {
+    public function isRouteValid(ControllerTester $I): void
+    {
+        $user = AdministratorFactory::createOne([
+            'email' => 'admin@example.com',
+            'password' => 'admin',
+            'roles' => ['ROLE_ADMIN'],
+        ]);
+        $realUser = $user->object();
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/fr/internship');
+        $I->seeResponseCodeIsSuccessful();
+    }
 }
