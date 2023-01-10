f17a604398bb60c7eaa72b5e35a5d46d1a6b3d92
laid0003 <lucie.laidie@etudiant.univ-reims.fr>
2023-01-10 21:45:12 +0100

    Ajout du test de la route de l'affichage des étudiants

---
+++ tests/Controller/Trombinoscope/TrombinoscopeCest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Controller\Trombinoscope;
+
+use App\Factory\AdministratorFactory;
+use App\Factory\StudentFactory;
+use App\Tests\Support\ControllerTester;
+
+class TrombinoscopeCest
+{
+    public function TestRouteStudent(ControllerTester $I)
+    {
+        $user = AdministratorFactory::createOne([
+            'email' => 'admin@example.com',
+            'password' => 'admin',
+            'roles' => ['ROLE_ADMIN'],
+        ]);
+        $realUser = $user->object();
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/fr/student');
+        $I->seeResponseCodeIsSuccessful();
+    }
+
+
+}
