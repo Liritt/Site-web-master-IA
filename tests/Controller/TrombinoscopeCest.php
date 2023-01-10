7dc7c6bc80c2f8bfa69b77f138c05b99b48b7117
laid0003 <lucie.laidie@etudiant.univ-reims.fr>
2023-01-10 21:12:53 +0100

    Création du fichier pour les tests sur le trombinoscope

---
+++ tests/Controller/TrombinoscopeCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+use App\Tests\Support\ControllerTester;
+
+class TrombinoscopeCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
